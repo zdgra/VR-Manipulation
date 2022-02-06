1ddf8d18e303409484db36e05d3b59c61bc3b576
Zachariah Abueg <76718657+zdgra@users.noreply.github.com>
2022-02-05 19:55:50 -0500

    committing project

---
+++ Assets/Scripts/BubbleCursorProvider.cs
@@ -0,0 +1,120 @@
+﻿/*
+*   Copyright (C) 2020 University of Central Florida, created by Dr. Ryan P. McMahan.
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*   Primary Author Contact:  Dr. Ryan P. McMahan <rpm@ucf.edu>
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+namespace UnityEngine.XR.Interaction.Toolkit
+{
+    // The BubbleCursorProvider works with the XRDirectInteractor to provide a 3D bubble cursor.
+    [AddComponentMenu("XRST/Interaction/BubbleCursorProvider")]
+    public class BubbleCursorProvider : MonoBehaviour
+    {
+        // The SphereCollider that the XRDirectInteractor uses for triggering. 
+        [SerializeField]
+        [Tooltip("The SphereCollider that the XRDirectInteractor uses for triggering.")]
+        SphereCollider m_Collider;
+        public SphereCollider Collider { get { return m_Collider; } set { m_Collider = value; } }
+
+        // The GameObject that the XRDirectInteractor uses to represent the 3D cursor. 
+        [SerializeField]
+        [Tooltip("The GameObject that the XRDirectInteractor uses to represent the 3D cursor.")]
+        GameObject m_Cursor;
+        public GameObject Cursor { get { return m_Cursor; } set { m_Cursor = value; } }
+
+        // The minimum radius that the cursor will shrink to. 
+        [SerializeField]
+        [Tooltip("The minimum radius that the cursor will shrink to.")]
+        float m_MinRadius = 0.1f;
+        public float MinRadius { get { return m_MinRadius; } set { m_MinRadius = value; } }
+
+        // The maximum radius that the cursor will grow to. 
+        [SerializeField]
+        [Tooltip("The maximum radius that the cursor will grow to.")]
+        float m_MaxRadius = 0.5f;
+        public float MaxRadius { get { return m_MaxRadius; } set { m_MaxRadius = value; } }
+
+        // Update is called once per frame.
+        void Update()
+        {
+            // If the collider and cursor are valid.
+            if (Collider != null && Cursor != null)
+            {
+                // Get a list of currently valid interactables. 
+                List<XRGrabInteractable> interactables = new List<XRGrabInteractable>(FindObjectsOfType<XRGrabInteractable>());
+
+                // Determine the closest interactable.
+                int closest = -1;
+                float closestDistance = Mathf.Infinity;
+
+                // For each interactable.
+                for (int i = 0; i < interactables.Count; i++)
+                {
+                    // Fetch the interactable's collider.
+                    Collider interactableCollider = interactables[i].GetComponent<Collider>();
+
+                    // Ensure the collider is valid.
+                    if (interactableCollider != null)
+                    {
+                        // Fetch the collider's bounds.
+                        Bounds interactableBounds = interactableCollider.bounds;
+
+                        // Determine its closest point to our collider.
+                        Vector3 closestPoint = interactableBounds.ClosestPoint(Collider.transform.position);
+
+                        // Determine its distance to our collider.
+                        float distance = Vector3.Distance(closestPoint, Collider.transform.position);
+
+                        // Buffer the distance to avoid non-collisions.
+                        distance += 0.01f;
+
+                        // Keep track of the closest distance and interactable.
+                        if (distance < closestDistance)
+                        {
+                            closest = i;
+                            closestDistance = distance;
+                        }
+                    }
+                }
+
+                // If a closest interactable was found and within the maximum radius.
+                if (closest > -1 && closestDistance <= MaxRadius)
+                {
+                    // If the closest distance is smaller than the minimum radius, reset it.
+                    if (closestDistance < MinRadius)
+                    {
+                        closestDistance = MinRadius;
+                    }
+
+                    // Set the collider and cursor to match the closest distance.
+                    Collider.radius = closestDistance;
+                    Cursor.transform.localScale = new Vector3(closestDistance * 2.0f, closestDistance * 2.0f, closestDistance * 2.0f);
+                }
+                // Otherwise, set the collider and cursor to the minimum radius.
+                else
+                {
+                    Collider.radius = MinRadius;
+                    Cursor.transform.localScale = new Vector3(MinRadius * 2.0f, MinRadius * 2.0f, MinRadius * 2.0f);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/OutlineProvider.cs
@@ -0,0 +1,217 @@
+﻿/*
+*   Copyright (C) 2020 University of Central Florida, created by Dr. Ryan P. McMahan.
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*   Primary Author Contact:  Dr. Ryan P. McMahan <rpm@ucf.edu>
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using cakeslice;
+
+namespace UnityEngine.XR.Interaction.Toolkit
+{
+    // The OutlineProvider works with an interactor to provide an outline for hovered and selected interactables.
+    [AddComponentMenu("XRST/Interaction/OutlineProvider")]
+    public class OutlineProvider : MonoBehaviour
+    {
+        // The interactor to provide outlines for.
+        [SerializeField]
+        [Tooltip("The interactor to provide outlines for.")]
+        XRBaseInteractor m_Interactor;
+        public XRBaseInteractor Interactor { get { return m_Interactor; } set { m_Interactor = value; } }
+
+        // The OutlineEffect to use for outlines.
+        [SerializeField]
+        [Tooltip("The OutlineEffect to use for outlines.")]
+        OutlineEffect m_OutlineEffect;
+        public OutlineEffect OutlineEffect { get { return m_OutlineEffect; } set { m_OutlineEffect = value; } }
+
+        // Whether the listeners have been added.
+        bool listenersAdded;
+
+        // Listener called by the interactor's OnHoverEnter interactor event.
+        public void OnHoverEnterOutline(XRBaseInteractable interactable)
+        {
+            // If an outline effect exists.
+            if (OutlineEffect != null)
+            {
+                // Attempt to get an outline component on the interactable.
+                Outline outline = interactable.GetComponent<Outline>();
+
+                // Create one if there isn't one already.
+                if (outline == null)
+                {
+                    outline = interactable.gameObject.AddComponent<Outline>();
+                }
+
+                // Set the outline's color to the line color 1 effect.
+                outline.color = 1;
+            }
+        }
+
+        // Listener called by the interactor's OnHoverExit interactor event.
+        public void OnHoverExitOutline(XRBaseInteractable interactable)
+        {
+            // If an outline effect exists.
+            if (OutlineEffect != null)
+            {
+                // Attempt to get an outline component on the interactable.
+                Outline outline = interactable.GetComponent<Outline>();
+
+                // Create one if there isn't one already.
+                if (outline == null)
+                {
+                    outline = interactable.gameObject.AddComponent<Outline>();
+                }
+
+                // Set the outline's color to the line color 0 effect.
+                outline.color = 0;
+            }
+        }
+
+        // Listener called by the interactor's OnSelectEnter interactor event.
+        public void OnSelectEnterOutline(XRBaseInteractable interactable)
+        {
+            // If an outline effect exists.
+            if (OutlineEffect != null)
+            {
+                // Attempt to get an outline component on the interactable.
+                Outline outline = interactable.GetComponent<Outline>();
+
+                // Create one if there isn't one already.
+                if (outline == null)
+                {
+                    outline = interactable.gameObject.AddComponent<Outline>();
+                }
+
+                // Set the outline's color to the line color 2 effect.
+                outline.color = 2;
+            }
+        }
+
+        // Listener called by the interactor's OnSelectExit interactor event.
+        public void OnSelectExitOutline(XRBaseInteractable interactable)
+        {
+            // If an outline effect exists.
+            if (OutlineEffect != null)
+            {
+                // Attempt to get an outline component on the interactable.
+                Outline outline = interactable.GetComponent<Outline>();
+
+                // Create one if there isn't one already.
+                if (outline == null)
+                {
+                    outline = interactable.gameObject.AddComponent<Outline>();
+                }
+
+                // Set the outline's color to the line color 0 effect.
+                outline.color = 0;
+            }
+        }
+
+        // Reset function for initializing the OutlineProvider.
+        void Reset()
+        {
+            // Attempt to fetch a local interactor.
+            m_Interactor = GetComponent<XRBaseInteractor>();
+
+            // Did not find a interactor.
+            if (m_Interactor == null)
+            {
+                // Warn the developer.
+                Debug.LogWarning("[" + gameObject.name + "][OutlineProvider]: Did not find a local interactor attached to the same game object.");
+
+                // Attepmt to fetch any interactor.
+                m_Interactor = FindObjectOfType<XRBaseInteractor>();
+
+                // Did not find one.
+                if (m_Interactor == null)
+                {
+                    Debug.LogWarning("[" + gameObject.name + "][OutlineProvider]: Did not find an interactor in the scene.");
+                }
+                // Found one.
+                else
+                {
+                    Debug.LogWarning("[" + gameObject.name + "][OutlineProvider]: Found an interactor attached to " + m_Interactor.gameObject.name + ".");
+                }
+            }
+
+            // Attempt to fetch a local OutlineEffect.
+            m_OutlineEffect = GetComponent<OutlineEffect>();
+
+            // Did not find an OutlineEffect.
+            if (m_OutlineEffect == null)
+            {
+                // Warn the developer.
+                Debug.LogWarning("[" + gameObject.name + "][OutlineProvider]: Did not find a local OutlineEffect attached to the same game object.");
+
+                // Attepmt to fetch any OutlineEffect.
+                m_OutlineEffect = FindObjectOfType<OutlineEffect>();
+
+                // Did not find one.
+                if (m_OutlineEffect == null)
+                {
+                    Debug.LogWarning("[" + gameObject.name + "][OutlineProvider]: Did not find an OutlineEffect in the scene.");
+                }
+                // Found one.
+                else
+                {
+                    Debug.LogWarning("[" + gameObject.name + "][OutlineProvider]: Found an OutlineEffect attached to " + m_OutlineEffect.gameObject.name + ".");
+                }
+            }
+        }
+
+        // This function is called when the behaviour becomes disabled.
+        void OnDisable()
+        {
+            // Attempt to remove the listeners from the events of the interactor.
+            if (m_Interactor != null && listenersAdded)
+            {
+                // Remove the OnHoverEnterOutline function as a listener.
+                m_Interactor.onHoverEnter.RemoveListener(OnHoverEnterOutline);
+                // Remove the OnHoverExitOutline function as a listener.
+                m_Interactor.onHoverExit.RemoveListener(OnHoverExitOutline);
+                // Remove the OnSelectEnterOutline function as a listener.
+                m_Interactor.onSelectEnter.RemoveListener(OnSelectEnterOutline);
+                // Remove the OnSelectExitOutline function as a listener.
+                m_Interactor.onSelectExit.RemoveListener(OnSelectExitOutline);
+                // Keep track of removing the listeners.
+                listenersAdded = false;
+            }
+        }
+
+        // This function is called when the object becomes enabled and active.
+        void OnEnable()
+        {
+            // Attempt to add the listeners to the events of the interactor.
+            if (m_Interactor != null && !listenersAdded)
+            {
+                // Add the OnHoverEnterOutline function as a listener.
+                m_Interactor.onHoverEnter.AddListener(OnHoverEnterOutline);
+                // Add the OnHoverExitOutline function as a listener.
+                m_Interactor.onHoverExit.AddListener(OnHoverExitOutline);
+                // Add the OnSelectEnterOutline function as a listener.
+                m_Interactor.onSelectEnter.AddListener(OnSelectEnterOutline);
+                // Add the OnSelectExitOutline function as a listener.
+                m_Interactor.onSelectExit.AddListener(OnSelectExitOutline);
+                // Keep track of adding the listeners.
+                listenersAdded = true;
+            }
+        }
+    }
+}
